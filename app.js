71a9ee4e8db58cf168adf1a1ef9b84bccd95431d
p.phonsing_ ✅ <129599660+Polsing@users.noreply.github.com>
2024-02-25 16:33:42 +0700

    beta v2.0

--- app.js
+++ app.js
@@ -23,7 +23,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
-app.use('/', registerRouter);
+app.get('/',(req,res)=>{
+  res.render('home');
+});
+
+app.use('/Register',registerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
